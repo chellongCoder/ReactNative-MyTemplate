1e7509c54021ba387970bb42181d5d6d0509f50b
LongNN <longnn@ows.com.vn>
2019-02-22 15:46:53 +0700

    Initial commit.

---
+++ .jest/cache/ts-jest/9e/1239ce9961a0126d822cabb687aa27ac0e7412/18a3dbc7012e66e19f4bf1cf93c0e2a1ad3785cf.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("react-native");
+var react_1 = __importDefault(require("react"));
+var App_1 = __importDefault(require("../App"));
+// Note: test renderer must be required after react-native.
+var react_test_renderer_1 = __importDefault(require("react-test-renderer"));
+it('renders correctly', function () {
+    react_test_renderer_1.default.create(react_1.default.createElement(App_1.default, null));
+});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJmaWxlIjoiL1VzZXJzL2NoZWxsb25nL1dvcmtzcGFjZS9SZWFjdE5hdGl2ZS9ob2NPbmxpbmVBQ1QvX190ZXN0c19fL0FwcC50c3giLCJtYXBwaW5ncyI6IjtBQUFBOzs7R0FHRzs7Ozs7QUFFSCx3QkFBc0I7QUFDdEIsZ0RBQTBCO0FBQzFCLCtDQUF5QjtBQUV6QiwyREFBMkQ7QUFDM0QsNEVBQTJDO0FBRTNDLEVBQUUsQ0FBQyxtQkFBbUIsRUFBRTtJQUN0Qiw2QkFBUSxDQUFDLE1BQU0sQ0FBQyw4QkFBQyxhQUFHLE9BQUcsQ0FBQyxDQUFDO0FBQzNCLENBQUMsQ0FBQyxDQUFDIiwibmFtZXMiOltdLCJzb3VyY2VzIjpbIi9Vc2Vycy9jaGVsbG9uZy9Xb3Jrc3BhY2UvUmVhY3ROYXRpdmUvaG9jT25saW5lQUNUL19fdGVzdHNfXy9BcHAudHN4Il0sInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogQGZvcm1hdFxuICogQGxpbnQtaWdub3JlLWV2ZXJ5IFhQTEFUSlNDT1BZUklHSFQxXG4gKi9cblxuaW1wb3J0ICdyZWFjdC1uYXRpdmUnO1xuaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcbmltcG9ydCBBcHAgZnJvbSAnLi4vQXBwJztcblxuLy8gTm90ZTogdGVzdCByZW5kZXJlciBtdXN0IGJlIHJlcXVpcmVkIGFmdGVyIHJlYWN0LW5hdGl2ZS5cbmltcG9ydCByZW5kZXJlciBmcm9tICdyZWFjdC10ZXN0LXJlbmRlcmVyJztcblxuaXQoJ3JlbmRlcnMgY29ycmVjdGx5JywgKCkgPT4ge1xuICByZW5kZXJlci5jcmVhdGUoPEFwcCAvPik7XG59KTtcbiJdLCJ2ZXJzaW9uIjozfQ==
